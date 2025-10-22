1e19047cb86730abb689ea3c0317eabe40347c44
MariskaPutri18 <mariskaputri960@gmail.com>
2025-10-22 14:05:26 +0700

    UTS: Selesai Soal 3 - Implementasi Flutter TTS

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,28 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'home_screen.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends StatefulWidget {
   final String ocrText;
 
   const ResultScreen({super.key, required this.ocrText});
 
   @override
+  State<ResultScreen> createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  late FlutterTts flutterTts;
+
+  @override
+  void initState() {
+    super.initState();
+    flutterTts = FlutterTts();
+    _initTts();
+  }
+
+  Future<void> _initTts() async {
+    await flutterTts.setLanguage("id-ID"); 
+    await flutterTts.setSpeechRate(0.9);   
+    await flutterTts.setPitch(1.0);       
+  }
+
+  Future<void> _speak() async {
+    if (widget.ocrText.isNotEmpty) {
+      await flutterTts.speak(widget.ocrText);
+    }
+  }
+
+  @override
+  void dispose() {
+    flutterTts.stop(); 
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Hasil OCR')),
+      appBar: AppBar(
+        title: const Text('Hasil OCR'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.volume_up),
+            onPressed: _speak, 
+          ),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: SelectableText(
-            ocrText.isEmpty
+            widget.ocrText.isEmpty
                 ? 'Tidak ada teks ditemukan.'
-                : ocrText,
+                : widget.ocrText, 
             style: const TextStyle(fontSize: 18),
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
+        onPressed: () {       
           Navigator.pushAndRemoveUntil(
             context,
             MaterialPageRoute(builder: (context) => const HomeScreen()),
